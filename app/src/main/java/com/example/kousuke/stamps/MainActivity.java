29f145d1493003748b3a18708d8ff496b56ac074
kousuke nezu <kousuke.n2132@gmail.com>
2017-11-18 21:06:02 +0900

    [add] アプリの基本作成

---
+++ app/src/main/java/com/example/kousuke/stamps/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.kousuke.stamps;
+
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.MapFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    MapFragment mf;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentV);
+
+        mf= MapFragment.newInstance();
+
+        FragmentManager fm = getFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.add(android.R.id.content, mf);
+        ft.commit();
+    }
+}
