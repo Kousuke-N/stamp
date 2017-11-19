6790ab7293ddf9745b48fffc408120cd1d16f2e9
kousuke nezu <kousuke.n2132@gmail.com>
2017-11-19 19:54:05 +0900

    [update] メイン画面をタブが使えるように変更

--- app/src/main/java/com/example/kousuke/stamps/MainActivity.java
+++ app/src/main/java/com/example/kousuke/stamps/MainActivity.java
@@ -2,6 +2,11 @@ package com.example.kousuke.stamps;
 
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,7 +14,7 @@ import android.util.Log;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity implements ViewPager.OnPageChangeListener {
 
     static MainActivity instance = null;
 
@@ -21,24 +26,53 @@ public class MainActivity extends AppCompatActivity{
         instance = this;
         setContentView(R.layout.activity_main);
 
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewPager);
+
+        MyFragmentPagerAdapter adapter = new MyFragmentPagerAdapter(getSupportFragmentManager());
+
+        viewPager.setAdapter(adapter);
+//        viewPager.addOnPageChangeListener(this);
+
+        tabLayout.setupWithViewPager(viewPager);
+
         googleApiConnector = new GoogleApiConnector();
         googleApiConnector.build(this);
     }
 
     @Override
-    protected void onStart(){
+    protected void onStart() {
         super.onStart();
         googleApiConnector.connect();
     }
 
     @Override
-    protected void onStop(){
+    protected void onStop() {
         googleApiConnector.disconnect();
         super.onStop();
     }
 
     // どこからでもMainActivityを参照できるようにするためのメソッド
-    static MainActivity getInstance(){
+    static MainActivity getInstance() {
         return instance;
     }
+
+    // *********************************************************************************************
+
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+    }
+
+    @Override
+    public void onPageSelected(int position) {
+        Log.d("MainActivity", "onPageSelected() position=" + position);
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int state) {
+
+    }
+
+    // *********************************************************************************************
 }
