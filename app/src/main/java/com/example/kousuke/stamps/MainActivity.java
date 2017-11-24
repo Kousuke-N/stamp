7aa4f8912cb6a054210682e768a45849aeac9805
kousuke nezu <kousuke.n2132@gmail.com>
2017-11-24 10:08:07 +0900

    [update] リストアクティビティ作成

--- app/src/main/java/com/example/kousuke/stamps/MainActivity.java
+++ app/src/main/java/com/example/kousuke/stamps/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.kousuke.stamps;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -10,6 +12,7 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
@@ -28,6 +31,15 @@ public class MainActivity extends AppCompatActivity implements ViewPager.OnPageC
 
         googleApiConnector = new GoogleApiConnector();
         googleApiConnector.build(this);
+
+        FloatingActionButton floatingActionButton = findViewById(R.id.list_button);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, StampList.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
---
+++ app/src/main/java/com/example/kousuke/stamps/StampList.java
@@ -0,0 +1,13 @@
+package com.example.kousuke.stamps;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StampList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stamp_list);
+    }
+}
