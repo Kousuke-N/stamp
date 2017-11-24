96fd15de8e6786d44ba829fae943c2e416f84db4
kousuke nezu <kousuke.n2132@gmail.com>
2017-11-24 22:29:26 +0900

    [update] 現在地を取得できるようにした

--- app/src/main/java/com/example/kousuke/stamps/MainActivity.java
+++ app/src/main/java/com/example/kousuke/stamps/MainActivity.java
@@ -1,15 +1,23 @@
 package com.example.kousuke.stamps;
 
+import android.Manifest;
 import android.content.Intent;
-import android.support.design.widget.FloatingActionButton;
-import android.support.v7.app.AppCompatActivity;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.app.AppCompatCallback;
 import android.support.v7.view.ActionMode;
-import android.view.View;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity implements AppCompatCallback{
+public class MainActivity extends AppCompatActivity implements AppCompatCallback {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +36,66 @@ public class MainActivity extends AppCompatActivity implements AppCompatCallback
                 startActivity(intent);
             }
         });
+
+        MyMapFragment myMapFragment = (MyMapFragment) getFragmentManager().findFragmentById(R.id.map_fragment);
+
+
+        // GPS(locationManager)の設定
+        // 権限がないとき
+        if (ActivityCompat.checkSelfPermission(getApplication(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(getApplication(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                    || ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_COARSE_LOCATION)) {
+                // TODO: ユーザに権限の説明が必要な場合
+                // ユーザが権限拒否やデバイスとして使えない場合falseを返すので注意
+            } else {
+                final int MY_PERMISSIONS_REQUEST_FINE_LOCATION = 1;
+                // 権限要求
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.READ_CONTACTS},
+                        MY_PERMISSIONS_REQUEST_FINE_LOCATION);
+            }
+
+        } else {
+            // locationManagerの設定
+            LocationManager locationManager= (LocationManager)getSystemService(LOCATION_SERVICE);
+            String locationProvider = null;
+
+            // GPSが利用可能かチェック
+            if(locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)){
+                locationProvider = LocationManager.GPS_PROVIDER;
+            }
+            // GPSプロバイダが有効になっていない場合
+            else if(locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)){
+                locationProvider = LocationManager.NETWORK_PROVIDER;
+            }
+            // いずれも利用可能でない場合はGPSを設定する画面に遷移
+            else{
+                Intent settingsIntent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(settingsIntent);
+                return;
+            }
+
+            // 位置情報を通知するための最小時間間隔（ミリ秒）
+            final long minTime = 500;
+            // 位置情報を通知するための最小距離間隔（メートル）
+            final long minDistance = 1;
+
+            // 利用可能なロケーションプロバイダによる位置情報の取得の開始
+            // FIXME: 本来であれば、リスナが複数回登録されないようにチェックする必要がある
+            locationManager.requestLocationUpdates(locationProvider, minTime, minDistance, myMapFragment);
+            // 最新の位置情報
+            Location location = locationManager.getLastKnownLocation(locationProvider);
+
+            if(location != null){
+                Log.i("MainActivity", location.getLatitude() + ":" + String.valueOf(location.getLongitude()));
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults){
 
     }
 
--- app/src/main/java/com/example/kousuke/stamps/MyMapFragment.java
+++ app/src/main/java/com/example/kousuke/stamps/MyMapFragment.java
@@ -1,5 +1,8 @@
 package com.example.kousuke.stamps;
 
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -8,5 +11,34 @@ import android.view.ViewGroup;
 
 import com.google.android.gms.maps.MapFragment;
 
-public class MyMapFragment extends MapFragment {
+public class MyMapFragment extends MapFragment
+        implements LocationListener{
+
+    // LocationListenerによるimplements ************************************************************
+
+    // 位置情報更新時
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    // ロケーションプロバイダが利用可能になった時
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    // ロケーションプロバイダが利用不可になった時
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    // ロケーションステータスが変化時
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+
+    // *********************************************************************************************
 }
