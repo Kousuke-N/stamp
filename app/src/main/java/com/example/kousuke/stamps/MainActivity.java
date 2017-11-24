5b93646dd5cd2a11fe6849ed37c186212ac26a7c
kousuke nezu <kousuke.n2132@gmail.com>
2017-11-24 20:37:16 +0900

    [update] メインとリストのツールバーとFABとページ遷移を実装

--- app/src/main/java/com/example/kousuke/stamps/MainActivity.java
+++ app/src/main/java/com/example/kousuke/stamps/MainActivity.java
@@ -1,18 +1,45 @@
 package com.example.kousuke.stamps;
 
-import android.support.v4.view.ViewPager;
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatCallback;
+import android.support.v7.view.ActionMode;
+import android.view.View;
+import android.support.v7.widget.Toolbar;
 
-import com.example.kousuke.stamps.Action.GoogleApiConnector;
-
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AppCompatCallback{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
         setContentView(R.layout.activity_main);
+
+        // ツールバーの設定
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_name);
+
+        FloatingActionButton fab = findViewById(R.id.fab_to_change_list);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ListActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    @Override
+    public void onSupportActionModeStarted(ActionMode mode) {
+        //let's leave this empty, for now
+        super.onSupportActionModeStarted(mode);
+    }
+
+    @Override
+    public void onSupportActionModeFinished(ActionMode mode) {
+        // let's leave this empty, for now
+        super.onSupportActionModeFinished(mode);
     }
 }
